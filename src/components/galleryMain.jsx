24a80200e344f086aa17e7c92e54d99eaa13f3ea
Emman <manugasemmanuell@gmail.com>
2023-07-14 20:47:55 +0800

    fourth commit

--- src/components/galleryMain.jsx
+++ src/components/galleryMain.jsx
@@ -68,8 +68,19 @@ export default function GalleryMain(){
                     </div>
                 ))}
             <div className="arrowContainer">
-                    <button onClick={handlePrevious} className="prevButton" style={{"visibility": handleArrow.current === 0? "hidden": "visible"}}>{'<'}</button>
-                    <button onClick={handleNext} className="nextButton" style={{"visibility": handleArrow.current === 3? "hidden": "visible"}}>{'>'}</button>
+                    
+                    <button class="arrow left prevButton" onClick={handlePrevious}  style={{"visibility": handleArrow.current === 0? "hidden": "visible"}}>
+                            <svg width="60px" height="80px" viewBox="0 0 50 80">
+                                <polyline fill="none" stroke="#FFFFFF" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="
+	                            45.63,75.8 0.375,38.087 45.63,0.375 "/>
+                            </svg>  
+                        </button>
+                        <button class="arrow right nextButton" onClick={handleNext}  style={{"visibility": handleArrow.current === 3? "hidden": "visible"}}>
+                            <svg width="60px" height="80px" viewBox="0 0 50 80">
+                                <polyline fill="none" stroke="#FFFFFF" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="
+	                            0.375,0.375 45.63,38.087 0.375,75.8 "/>
+                            </svg>
+                        </button>
             </div>
             </div>
         </div>
