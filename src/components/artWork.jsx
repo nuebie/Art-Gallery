c412aba76c494550300e3c5bc5292364d0c29920
Emman <manugasemmanuell@gmail.com>
2023-07-14 19:38:03 +0800

    third commit

--- src/components/artWork.jsx
+++ src/components/artWork.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function ArtWork({imgSrc}){
+export default function ArtWork({imgSrc, title, description}){
     return(
-        <div>
-            <img src={imgSrc} alt="" />
+        <div className="artWork">
+            <img src={imgSrc} alt="" className="art"/>
+            <div className="callOut">
+                <div className="artDesc">
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </div>
+                <div className="connector"></div>
+            </div>
         </div>
     )
 }
--- src/components/galleryMain.jsx
+++ src/components/galleryMain.jsx
@@ -1,14 +1,77 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import ArtworkObjects from "./artGalleryData";
 import ArtWork from "./artWork";
 
 
 export default function GalleryMain(){
+    const arr = useRef([0,0,0,0]);
+    //const [handleArrow,setHandleArrow] = useState(0);
+    const handleArrow = useRef(0);
+
+    const handlePrevious = () => {
+        //setHandleArrow(current => current-1);
+        handleArrow.current = handleArrow.current - 1;
+        console.log(handleArrow.current);
+        arr.current.forEach((val,index) =>{
+            //const element = document.querySelector(`.artWorkWrapper+${index}`);
+            const newPosX = val + (100);
+            arr.current[index] = newPosX;
+            document.querySelector(`.slide${index}`).style.setProperty('--i', newPosX);
+        })
+        if(handleArrow.current === 0){
+            document.querySelector('.prevButton').style.visibility = 'hidden';
+        }
+        else{
+            document.querySelector('.prevButton').style.visibility = 'visible';
+        }
+        if(handleArrow.current === 3){
+            document.querySelector('.nextButton').style.visibility = 'hidden';
+        }
+        else{
+            document.querySelector('.nextButton').style.visibility = 'visible';
+        }
+      };
+    
+      const handleNext = () => {
+        //setHandleArrow(current => current+1);
+        handleArrow.current = handleArrow.current + 1;
+        console.log(handleArrow.current);
+        arr.current.forEach((val,index) =>{
+            //const element = document.querySelector(`.artWorkWrapper+${index}`);
+            const newPosX = val + (-100);
+            arr.current[index] = newPosX;
+            document.querySelector(`.slide${index}`).style.setProperty('--i', newPosX);
+        })
+        if(handleArrow.current === 0){
+            document.querySelector('.prevButton').style.visibility = 'hidden';
+        }
+        else{
+            document.querySelector('.prevButton').style.visibility = 'visible';
+        }
+        if(handleArrow.current === 3){
+            document.querySelector('.nextButton').style.visibility = 'hidden';
+        }
+        else{
+            document.querySelector('.nextButton').style.visibility = 'visible';
+        }
+      };
+
+
     return(
-        <div>
-            {ArtworkObjects.map(artObj => (
-                <ArtWork imgSrc={artObj.src} key={artObj.id}/>
-            ))}
+        <div className="gallerySection">
+            <h1 className="logoText">アート</h1>
+            <div className="spotlight"></div>
+            <div className="artWorkWrapper">
+                {ArtworkObjects.map((artObj,index) => (
+                    <div className={`artWorkContainer slide${index}`} id="artWorkContainer" style={{ '--i': `${arr.current[index]}%`}}>
+                         <ArtWork imgSrc={artObj.src} title={artObj.title} description={artObj.description} key={artObj.id}/>
+                    </div>
+                ))}
+            <div className="arrowContainer">
+                    <button onClick={handlePrevious} className="prevButton" style={{"visibility": handleArrow.current === 0? "hidden": "visible"}}>{'<'}</button>
+                    <button onClick={handleNext} className="nextButton" style={{"visibility": handleArrow.current === 3? "hidden": "visible"}}>{'>'}</button>
+            </div>
+            </div>
         </div>
     )
 } 
